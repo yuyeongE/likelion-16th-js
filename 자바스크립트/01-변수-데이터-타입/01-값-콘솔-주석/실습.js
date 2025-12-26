65e12f621aca6140ad35363bf6a124f9d000f6fc
yuyeougE <yyoung2404@naver.com>
2025-12-26 13:42:07 +0900

    [200~학습: JavaScript 데이터 타입 (숫자, 문자, 불리언)

--- 자바스크립트/01-변수-데이터-타입/01-값-콘솔-주석/실습.js
+++ 자바스크립트/01-변수-데이터-타입/01-값-콘솔-주석/실습.js
@@ -1,11 +1,23 @@
-// 숫자(number) 값
+//숫자(number) 값 (number value or literal)
 
-// 문자(string) 값
+//문자(string) 값
+//작은 따옴표(')
+//큰 따옴표(")
+//역 따옴표(`, backtick)
 
-// 논리(boolean) 값
+//논리(boolean) 값
+// truthy X
+// falsy X
+// yes / no X
+// ok X 프로그램이 허용하지 않는 문자(정의되지 않은)
+true;
+false; //프로그램이 허용하는 불리언 값
 
-// 터미널에 값 출력
+//터미널에 값 출력
 
-// 한 줄 (single comment)
+//주석 한 줄 (single comment)
 
-// 여러 줄 (multiple comments)
+/*여러 줄
+주석 작성 (multiple comments)
+CSS 주석과 유사
+*/
